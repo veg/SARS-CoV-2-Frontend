95f527fe107fcde5bbc3b769b705d204afa296b2
Steven Weaver <sweaver@temple.edu>
2021-09-10 17:17:01 -0700

    detailed report

--- src/routes/lineage/[slug].svelte
+++ src/routes/lineage/[slug].svelte
@@ -5,25 +5,33 @@
 </script>
 
 <script>
+
   import { onMount } from "svelte";
 	import * as R from "ramda";
 
 	export let lineage;
 	let dagData = [];
 	let taskData = [];
+  let groupedTaskData = {'fill': {}};
 
-  // selection_analyses_nsp2.meme_full
   let genes = ["nsp3","nsp4","3C","nsp6","nsp7","nsp8","nsp9","nsp10","helicase","exonuclease","endornase","S","E","M","N","ORF3a","ORF6","ORF7a","ORF8","RdRp","methyltransferase"]
   let methods = ["meme_full","fade","fel","relax","bgm","busted","meme","prime","cfel","absrel","slac"]
+  // selection_analyses_nsp2.meme_full
   let taskNames = genes.flatMap(d => methods.map(v => "selection_analyses_" + d + "." + v))
 
+  function getStateBGColor(task) {
+
+    if(task.state == "success") {
+      return "bg-green-400"
+    } else {
+      return "bg-red-400"
+    }
+
+  }
 
   onMount(async () => {
 
     let url = "http://silverback.temple.edu/veg/airflow/api/v1/dags/rascl_" + lineage + "/dagRuns"
-    // let url = "http://silverback.temple.edu/veg/airflow/api/v1/dags/rascl_" + lineage + "/dagRuns/scheduled__2021-08-22T00:00:00+00:00/taskInstances?limit=500"
-    let username = "datamonkey";
-    let password = "0cc4m5R0ck5";
     let dagRunId = '';
 
     // Get dagRun id of latest completed run (state not running)
@@ -59,34 +67,96 @@
     const taskRawData = await taskRes.json();
 
     if (taskRes.status === 200) {
+
       taskData = R.filter(d=> R.includes(d.task_id, taskNames), taskRawData.task_instances)
-      console.log(taskData);
+      // Create results url
+      // http://data.hyphy.org/web/covid-19/selection-analyses/rascl/AY.12/scheduled__2021-08-22T00%3A00%3A00%2B00%3A00/sequences.N.MEME.json
+      let baseResultsUrl = "http://data.hyphy.org/web/covid-19/selection-analyses/rascl/" + lineage + "/" + dagRunId + "/";
+
+      R.forEach(d => {
+          let [gene, method] = d.task_id.split('_')[2].split('.');
+          d.resultsUrl = baseResultsUrl + 'sequences.' + gene + '.' + R.toUpper(method) + '.json';
+          d.hyphyVisionUrl = "http://vision.hyphy.org/" + method + "?resultsUrl=" + encodeURIComponent(d.resultsUrl);
+          d.gene = gene;
+          d.method = method;
+      }, taskData);
+
+      groupedTaskData = R.groupBy(d => d.gene, taskData);
+      groupedTaskData = R.map(g => R.indexBy(d => d.method, g), groupedTaskData)
+      console.log(groupedTaskData);
+
     } else {
+
       console.log(taskRes.status, taskRawData.message);
       return;
+
     }
-
-
 
   });
 
 </script>
 
-<style>
+<style global lang="postcss">
+
+	h1 {
+		text-align: center;
+		margin: 0;
+  }
+
+	h2 {
+		text-align: center;
+		margin: 0;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+  h2 {
+		font-size: 1.4em;
+		text-transform: uppercase;
+		font-weight: 100;
+		margin: 0 0 0.5em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+
 </style>
 
 <svelte:head>
 	<title>Lineage</title>
 </svelte:head>
 
-<h1>{lineage}</h1>
+<h2>{lineage}</h2>
+
+<h1>Summary</h1>
+
+<h1>Individual Results</h1>
 
 <div class="content">
-  <table>
-  {#each taskData as task}
-    <tr>
-      <td>{task.task_id}</td><td>{task.state}</td>
-    </tr>
+  {#each Object.keys(groupedTaskData) as group}
+    <h2>{ group }</h2>
+    <div class="grid gap-4 grid-cols-3">
+      {#each R.sortBy(R.prop('method'))(Object.values(groupedTaskData[group])) as task}
+        <div class="grid grid-rows-2 grid-flow-col place-items-center { getStateBGColor(task) }">
+          <div class="col-span-2 bg-white text-center w-full h-full p-2">{ task.method }</div>
+          {#if task.state == "success"}
+            <div class="row-span-1 text-center w-full border-r-2 border-black"><a href={task.hyphyVisionUrl}>Visualization</a></div>
+            <div class="row-span-1 text-center w-full"><a href={task.resultsUrl}>Export</a></div>
+          {:else}
+            <div class="col-span-2 row-span-1 text-center w-full">Not Available</div>
+          {/if}
+        </div>
+      {/each}
+    </div>
   {/each}
-  </table>
+
 </div>
+
